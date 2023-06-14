1b1839cb17ba193164d2477febf46efc305e1f19
swetha-sivasankaran <swetha.sivasankaran@aspiresys.com>
2023-06-14 10:23:36 +0530

    Modified Code

---
+++ Aspire.Assignment/Assignment.Contracts/DTO/UserTokenDTO.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Contracts.DTO
+{
+    public class UserTokenDTO
+    {
+      //  public int Id { get; set; }
+        public string Token { get; set; }
+        public string? Roles { get; set; }
+        public int EmpDetailsID{get; set;}
+
+
+    }
+}
